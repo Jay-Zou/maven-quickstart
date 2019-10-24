5a7676dc25fb4541d6f264d47eb5a11a5b7807c1
demojie <1426134543@qq.com>
2019-10-24 23:37:35 +0800

    'Init'

---
+++ src/main/java/cn/demojie/App.java
@@ -0,0 +1,13 @@
+package cn.demojie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
